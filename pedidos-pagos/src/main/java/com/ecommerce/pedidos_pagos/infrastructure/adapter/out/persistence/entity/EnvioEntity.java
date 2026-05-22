34ea0e9efe42e41914e61ed6b404690bf82a4274
gasnito630-hash <gasnito630@gmail.com>
2026-05-22 05:06:05 -0500

    Update EnvioEntity.java

--- pedidos-pagos/src/main/java/com/ecommerce/pedidos_pagos/infrastructure/adapter/out/persistence/entity/EnvioEntity.java
+++ pedidos-pagos/src/main/java/com/ecommerce/pedidos_pagos/infrastructure/adapter/out/persistence/entity/EnvioEntity.java
@@ -5,27 +5,33 @@ import lombok.Data;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "Envio")
+@Table(name = "envio")
 @Data
 public class EnvioEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "EnvioID")
+    @Column(name = "envioid")
     private Long envioId;
 
-    @Column(name = "PedidoID")
+    @Column(name = "pedidoid", nullable = false)
     private Long pedidoId;
-    @Column(name = "MetodoEnvioID")
+
+    @Column(name = "metodoenvioid")
     private Long metodoEnvioId;
-    @Column(name = "NumeroTracking", length = 100)
+
+    @Column(name = "numerotracking", length = 100)
     private String numeroTracking;
-    @Column(name = "EstadoEnvio", length = 50)
+
+    @Column(name = "estadoenvio", length = 50)
     private String estadoEnvio;
-    @Column(name = "FechaEstimadaEntrega")
+
+    @Column(name = "fechaestimadaentrega")
     private LocalDateTime fechaEstimadaEntrega;
-    @Column(name = "FechaEnvio")
+
+    @Column(name = "fechaenvio")
     private LocalDateTime fechaEnvio;
-    @Column(name = "FechaEntrega")
+
+    @Column(name = "fechaentrega")
     private LocalDateTime fechaEntrega;
 
     @PrePersist
